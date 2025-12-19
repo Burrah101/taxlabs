7c00929de2a4c9bfd3458334d3d8a34539675fa7
Burrah101 <sansingh77@gmail.com>
2025-12-19 00:37:17 -0800

    Update app.py
    api key swaped

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 print("✅ This is the correct app.py")
 
 # ✅ Stripe Secret Key (paste the fresh sk_test key here)
-stripe.api_key = "sk_test_51sfuJ1GXW2HJur5PnDM1nTbr5P96PAUKWdpCe7gjiNzf9Mj3XE6xrWJ91cQpH8oy63y9j7A0yJ1koMSCQyTnCCTbo1h3t4XARLg100kUAsqDKPr"
+stripe.api_key = "sk_test_51SfuJ1GXW2HJur5PG9FAUKWdpCe79jlnXMj3XE6xrWJ91cQpH80y6Jy9j7A0yJlkoM5CQyTnC0Tbo1h3t4XRALg100KuASqDKr"
 
 YOUR_PRICE = 300  # in cents ($3.00)
 
